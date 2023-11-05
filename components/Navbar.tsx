9e59a6e5fd38feab358f7f18f24907c203c8f220
RafaGonzalezDev <rafagonzalezdeveloper@gmail.com>
2023-11-05 20:09:43 +0100

    Update Navbar component and package dependencies.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,28 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { NAV_LINKS } from '@/constants';
+import Button from './Button';
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-between max-container padding-container relative z-30 py-5">
+        <nav className="flex flexBetween max-container padding-container relative z-30 py-5">
             <Link href="/">
-                <Image src="/hilink-logo.svg" alt='logo' width={74} height={29} />
+                <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
             </Link>
 
             <ul className="hidden h-full gap-12 lg:flex">
-                
+                {NAV_LINKS.map((link) => {
+                    return (
+                        <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flex flex-center cursor-pointer pb-1.5 transition-all hover:font-bold'>
+                            {link.label}
+                        </Link>
+                    );
+                })}
             </ul>
+
+            <div className='lg:flexCenter hidden'>
+                <Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green" />
+            </div>
         </nav>
     );
 };
