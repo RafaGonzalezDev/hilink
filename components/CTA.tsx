4a6bf4f6745d84e28e8df7911600b2ca14e17080
RafaGonzalezDev <rafagonzalezdeveloper@gmail.com>
2023-11-07 06:11:37 +0100

    Add cursor pointer to button label and update CTA section with app store and play store buttons.

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import Image from 'next/image';
 
 const CTA = () => {
     return (
@@ -8,9 +9,13 @@ const CTA = () => {
                 <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
                     <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
                     <p className="regular-16 text-gray-10">Available on iOS and Android</p>
-                    <div>
-                        <Button type='button' title='Download App' icon='/apple.svg' variant='btn_white' full />
+                    <div className='flex w-full flex-col gap-3 white-space-nowrap xl:flex-row'>
+                        <Button type='button' title='App Store' icon='/apple.svg' variant='btn_white' full />
+                        <Button type='button' title='Play Store' icon='/android.svg' variant='btn_dark_green_outline' full />
                     </div>
+                </div>
+                <div className="flex flex-1 items-center justify-end">
+                    <Image src="/phones.png" alt="phones" width={550} height={870} />
                 </div>
             </div>
         </section>
